7963946a62785d4c8c41e83cc2fc122f6f2f1416
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-03-15 14:38:00 +0530

    Request Loan

--- script.js
+++ script.js
@@ -165,3 +165,17 @@ btnTransfer.addEventListener('click', function (e) {
     updateUI(currentAccount)
   }
 });
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add Movement...
+    currentAccount.movements.push(amount);
+
+    // Update UI...
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
